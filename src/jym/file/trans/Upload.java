ed3c7f117a48e1272bcb3abcc6e1733d98b29aac
CatfoOD <yanming-sohu@sohu.com>
2014-05-08 08:50:06 +0800

    Signed-off-by: CatfoOD <yanming-sohu@sohu.com>

--- src/jym/file/trans/Upload.java
+++ src/jym/file/trans/Upload.java
@@ -3,6 +3,10 @@
 package jym.file.trans;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 import javax.faces.bean.ApplicationScoped;
 import javax.faces.bean.ManagedBean;
@@ -11,6 +15,7 @@ import jym.file.FaceBase;
 import jym.file.SecurityManager;
 import jym.file.Util;
 import jym.file.tree.Directory;
+import jym.sim.util.Tools;
 
 import org.apache.commons.fileupload.disk.DiskFileItem;
 
@@ -35,11 +40,34 @@ public class Upload extends FaceBase implements IUpFileProcesser {
 			File currDir = dir.getRoot();
 			if (currDir == null) return;
 			
-			File target = new File(currDir.getPath() + "/" + item.getName());
-			item.getStoreLocation().renameTo(target);
+			File remoteName = new File(item.getName());
+			String fileName = remoteName.getName();
+		
+			File target = new File(currDir.getPath() + "/" + fileName);
+			if (!item.getStoreLocation().renameTo(target)) {
+				Tools.pl("move file fail, copy that.");
+				copy(item, target);
+			}
 			dir.refresh();
 		} catch(Exception e) {
 			e.printStackTrace();
+		}
+	}
+	
+	private void copy(DiskFileItem item, File target) throws IOException {
+		InputStream in = item.getInputStream();
+		OutputStream out = new FileOutputStream(target);
+		
+		try {
+			byte[] buff = new byte[256];
+			int readlen = in.read(buff);
+			while (readlen >= 0) {
+				out.write(buff, 0, readlen);
+				readlen = in.read(buff);
+			}
+		} finally {
+			in.close();
+			out.close();
 		}
 	}
 }
